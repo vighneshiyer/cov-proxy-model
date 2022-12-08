3209fbec40f3ac876385376152d40090fe6e325d
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2022-12-08 02:00:14 +0000

    use joblib to parallelize elf generation and spike calling

--- scripts/generate_elves.py
+++ scripts/generate_elves.py
@@ -1,5 +1,6 @@
 import argparse
 from pathlib import Path
+from joblib import delayed, Parallel
 
 from modeling.commands import run_cmd
 
@@ -11,13 +12,18 @@ def main():
     parser.add_argument('--config-dir', help="Path to a directory of config JSON objects", required=True)
     args = parser.parse_args()
 
-    riscv_dv_path = (Path.cwd() / "riscv-dv").resolve()
     riscv_dv_bin = Path(args.riscv_dv_bin).resolve()
 
     config_dir = Path(args.config_dir)
+    programs = []
     for config_id in config_dir.iterdir():
         assert config_id.is_dir()
         for config_seed in config_id.glob("*.json"):
-            cmd = ["generate_elf", "--riscv-dv-bin", str(riscv_dv_bin), "--config", config_seed.resolve(),
-                   "--out-dir", str(config_id), "--out-prefix", config_seed.name.split('.')[0]]
-            stdout, stderr, timeout = run_cmd(cmd, {}, timeout=None, check_return_code=False)  # failures are handled later
+            programs.append(delayed(call_generate_elf)(riscv_dv_bin, config_id, config_seed))
+    Parallel(n_jobs=24, prefer="threads")(programs)
+
+
+def call_generate_elf(riscv_dv_bin: Path, config_id: Path, config_seed: Path) -> None:
+    cmd = ["generate_elf", "--riscv-dv-bin", str(riscv_dv_bin), "--config", config_seed.resolve(),
+           "--out-dir", str(config_id), "--out-prefix", config_seed.name.split('.')[0]]
+    stdout, stderr, timeout = run_cmd(cmd, {}, timeout=None, check_return_code=False)  # failures are handled later
