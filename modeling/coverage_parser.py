f20cd9e627d2660826beb2c41e5d2355d8687f1c
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2022-12-09 17:10:19 -0800

    parse cov.dat

--- modeling/coverage_parser.py
+++ modeling/coverage_parser.py
@@ -11,26 +11,20 @@ from typing import Optional, Tuple
 # - CoverageEntry(Test1Module.sv,8,List(c0),0)
 # - CoverageEntry(Test1Module.sv,8,List(c1),0)
 
-cov_t = Tuple[str, int, str, int]  # file, line, path, count
+cov_t = Tuple[str, int, str, str, str, int]  # file, line, module, type, path, count
 
 
 def parse_line(line: str) -> Optional[cov_t]:
-    if line.startswith("C '\u0001"):
-        return None
-    #line.split('\'').toList match {
-    #    case List(_, dict, countStr) =>
-    #val entries = dict.drop(1).split('\u0001').map(_.split('\u0002').toList).map { case Seq(k, v) => k -> v }.toMap
-    #val count = countStr.trim.toLong
-    #val path = entries("h").split('.').toList.drop(2).mkString(".")
-    #val kind = entries("page").split("/").head
-    #val cov = CoverageEntry(file = entries("f"), line = entries("l").toInt, path = path, count = count)
-    #          // filter out non-user coverage
-    #kind match {
-    #    case "v_user" => Some(cov)
-    #case _        => None
-    #}
-    #case _ =>
-    #throw new RuntimeException(s"Unexpected coverage line format: $line")
-    #}
-    #}
+    sections_large = line.split(' ')
+    assert sections_large[0] == "C"
+    count = int(sections_large[2])
 
+    data = sections_large[1].split('\u0001')[1:]
+
+    file = data[0].split('\u0002')[1]
+    line = int(data[1].split('\u0002')[1])
+    module = data[3].split('\u0002')[1].split('/')[1]
+    tpe = data[4].split('\u0002')[1]
+    path = data[-1].split('\u0002')[1][:-1]
+
+    return file, line, module, tpe, path, count
--- tests/test_coverage_parser.py
+++ tests/test_coverage_parser.py
@@ -1,4 +1,36 @@
 from modeling.coverage_parser import parse_line
 
 def test_parse_line():
-    parse_line("asdf")
+    line1 = "C 'f/nscratch/vighneshiyer/cov-proxy-model/rocket-chip/emulator/generated-src/freechips.rocketchip.system.DefaultRV32Config.behav_srams.vl129n3pagev_line/tag_array_0_extoblockS129-130hTOP.TestHarness.ldut.tile_prci_domain.tile_reset_domain_tile.frontend.icache.tag_array.tag_array_0_ext' 160132"
+    line2 = "C 'f/nscratch/vighneshiyer/cov-proxy-model/rocket-chip/emulator/generated-src/freechips.rocketchip.system.DefaultRV32Config.vl935n14pagev_branch/TLXbaroifS935-936hTOP.TestHarness.ldut.subsystem_sbus.system_bus_xbar' 160122"
+    line3 = "C 'f/nscratch/vighneshiyer/cov-proxy-model/rocket-chip/emulator/generated-src/freechips.rocketchip.system.DefaultRV32Config.vl935n15pagev_branch/TLXbaroelsehTOP.TestHarness.ldut.subsystem_sbus.system_bus_xbar' 0"
+
+    p1 = parse_line(line1)
+    assert p1 == (
+        "/nscratch/vighneshiyer/cov-proxy-model/rocket-chip/emulator/generated-src/freechips.rocketchip.system.DefaultRV32Config.behav_srams.v",
+        129,
+        "tag_array_0_ext",
+        "block",
+        "TOP.TestHarness.ldut.tile_prci_domain.tile_reset_domain_tile.frontend.icache.tag_array.tag_array_0_ext",
+        160132
+    )
+
+    p2 = parse_line(line2)
+    assert p2 == (
+        "/nscratch/vighneshiyer/cov-proxy-model/rocket-chip/emulator/generated-src/freechips.rocketchip.system.DefaultRV32Config.v",
+        935,
+        "TLXbar",
+        "if",
+        "TOP.TestHarness.ldut.subsystem_sbus.system_bus_xbar",
+        160122
+    )
+
+    p3 = parse_line(line3)
+    assert p3 == (
+        "/nscratch/vighneshiyer/cov-proxy-model/rocket-chip/emulator/generated-src/freechips.rocketchip.system.DefaultRV32Config.v",
+        935,
+        "TLXbar",
+        "else",
+        "TOP.TestHarness.ldut.subsystem_sbus.system_bus_xbar",
+        0
+    )
