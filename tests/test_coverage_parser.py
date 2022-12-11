30db8dd1784cd5c8c2d5f0188b4506731e26d6d0
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2022-12-11 23:34:13 +0000

    parsed all test data into notebook

--- tests/test_coverage_parser.py
+++ tests/test_coverage_parser.py
@@ -1,5 +1,6 @@
 from modeling.coverage_parser import parse_line
 
+
 def test_parse_line():
     line1 = "C 'f/nscratch/vighneshiyer/cov-proxy-model/rocket-chip/emulator/generated-src/freechips.rocketchip.system.DefaultRV32Config.behav_srams.vl129n3pagev_line/tag_array_0_extoblockS129-130hTOP.TestHarness.ldut.tile_prci_domain.tile_reset_domain_tile.frontend.icache.tag_array.tag_array_0_ext' 160132"
     line2 = "C 'f/nscratch/vighneshiyer/cov-proxy-model/rocket-chip/emulator/generated-src/freechips.rocketchip.system.DefaultRV32Config.vl935n14pagev_branch/TLXbaroifS935-936hTOP.TestHarness.ldut.subsystem_sbus.system_bus_xbar' 160122"
