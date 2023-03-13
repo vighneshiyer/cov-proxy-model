b5a862788bfdccff83fc07fc79650dc0073611cb
Eran Kohen <erankohen@berkeley.edu>
2023-03-13 13:58:43 -0700

    added coverage parser

--- scripts/combined_scripts.py
+++ scripts/combined_scripts.py
@@ -6,6 +6,7 @@ import argparse
 from generate_configs import generate_configs
 from generate_elves import generate_elves
 from run_all_rtl_sims import run_all_rtl_sims
+from parse_coverages import parse_coverages
 
 def main():
     parser = argparse.ArgumentParser(prog="combined_scripts",
@@ -16,15 +17,16 @@ def main():
                         help="The probability that each plusarg will be the default value", required=True)
     parser.add_argument('--riscv-dv-bin', help="The riscv-dv compiled binary", required=True)
     parser.add_argument('--rtl-sim-bin', help="The compiled Rocket-Chip rv32imc Verilator simulation binary", required=True)
-    parser.add_argument('--out-dir', help="Path to a directory to store configs and outputs in", required=True)
+    parser.add_argument('--config-dir', help="Path to a directory to store configs, elves, and simulation logs in", required=True)
+    parser.add_argument('--out-dir', help="Path to a directory to store output CSVs in", required=True)
     args = parser.parse_args()
 
-    generate_configs(args.out_dir, args.n_configs, args.n_seeds, args.default_prob)
-    generate_elves(args.riscv_dv_bin, args.out_dir)
+    generate_configs(args.config_dir, args.n_configs, args.n_seeds, args.default_prob)
+    generate_elves(args.riscv_dv_bin, args.config_dir)
     # TODO: figure out how to handle elf timeout errors.
-    run_all_rtl_sims(args.rtl_sim_bin, args.out_dir)
+    run_all_rtl_sims(args.rtl_sim_bin, args.config_dir)
     # TODO: figure out how to handle rtl simulation errors.
-    # TODO: run parse_coverages.py as well. May need to change --out-dir to --config-dir and use --out-dir for pandas db output.
+    parse_coverages(args.config_dir, args.out_dir)
 
 if __name__ == "__main__":
     main()
--- scripts/parse_coverages.py
+++ scripts/parse_coverages.py
@@ -1,2 +1,49 @@
-# TODO: parse the coverage files and generate a pandas db. Store the db in a file.
-# TODO: parallelize the parsing of the coverage files
+from pathlib import Path
+import json
+import argparse
+from modeling.coverage_parser import parse_line
+
+def parse_file(config_dir, config_num, seed_num, out_dir):
+    cov_file = Path(config_dir) / f"{config_num}" / f"{seed_num}.cov.dat"
+    conf_file = Path(config_dir) / f"{config_num}" / f"{seed_num}.json"
+    conf_f = open(conf_file, "r")
+    configs = json.load(conf_f)['plusarg_config']
+    conf_f.close()
+    f = open(cov_file, "r")
+    lines = f.readlines()[1:]
+    for idx, line in enumerate(lines):
+        count = parse_line(line)[-1]
+        path = Path(out_dir) / f"{idx}.csv"
+        if not path.exists():
+            path.touch()
+            config_names = ','.join(configs.keys())
+            path.write_text(f"{config_names},count\n")
+        with open(path, "a") as out_f:
+            config_values = ','.join(configs.values())
+            out_f.write(f"{config_values},{count}\n")
+    f.close()
+
+def parse_coverages(config_dir, out_dir):
+    # TODO: parallelize this
+    out_dir = Path(out_dir)
+    if not out_dir.exists():
+        out_dir.mkdir()
+    config_dir = Path(config_dir)
+    for config_id in config_dir.iterdir():
+        assert config_id.is_dir()
+        for seed in config_id.glob("*.cov.dat"):
+            seed_num = seed.name.replace(".cov.dat", "")
+            config_num = config_id.name
+            parse_file(config_dir, config_num, seed_num, out_dir)
+
+def main():
+    parser = argparse.ArgumentParser(prog="parse_coverages",
+                                     description="Parse coverage files and generate csv files")
+    parser.add_argument('--config-dir', help="Path to a directory of coverage files", required=True)
+    parser.add_argument('--out-dir', help="Path to a directory to store the output csvs", required=True)
+    args = parser.parse_args()
+
+    parse_coverages(args.config_dir, args.out_dir)
+
+if __name__ == "__main__":
+    main()
