b2118ef5e95544c6e190dcedd3cffad821ee4f9a
Eran Kohen <erankohen@berkeley.edu>
2023-03-17 18:59:27 -0700

    coverage parser now does not output csv for mostly covered points

--- scripts/parse_coverages.py
+++ scripts/parse_coverages.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 import json
 import argparse
+# import pandas as pd
+import numpy as np
 from modeling.coverage_parser import parse_line
 
-def parse_file(config_dir, config_num, seed_num, out_dir):
+def parse_file(config_dir, config_num, seed_num, out_arrays):
     cov_file = Path(config_dir) / f"{config_num}" / f"{seed_num}.cov.dat"
     conf_file = Path(config_dir) / f"{config_num}" / f"{seed_num}.json"
     conf_f = open(conf_file, "r")
@@ -13,28 +15,29 @@ def parse_file(config_dir, config_num, seed_num, out_dir):
     lines = f.readlines()[1:]
     for idx, line in enumerate(lines):
         count = parse_line(line)[-1]
-        path = Path(out_dir) / f"{idx}.csv"
-        if not path.exists():
-            path.touch()
-            config_names = ','.join(configs.keys())
-            path.write_text(f"{config_names},count\n")
-        with open(path, "a") as out_f:
-            config_values = ','.join(configs.values())
-            out_f.write(f"{config_values},{count}\n")
+        if idx not in out_arrays:
+            out_arrays[idx] = np.expand_dims(np.array(list(configs.keys()) + ["count"]), 0)
+        out_arrays[idx] = np.append(out_arrays[idx], [list(configs.values()) + [count]], axis=0)
     f.close()
 
 def parse_coverages(config_dir, out_dir):
-    # TODO: parallelize this
-    out_dir = Path(out_dir)
-    if not out_dir.exists():
-        out_dir.mkdir()
+    out_arrays = {}
     config_dir = Path(config_dir)
     for config_id in config_dir.iterdir():
         assert config_id.is_dir()
         for seed in config_id.glob("*.cov.dat"):
             seed_num = seed.name.replace(".cov.dat", "")
             config_num = config_id.name
-            parse_file(config_dir, config_num, seed_num, out_dir)
+            parse_file(config_dir, config_num, seed_num, out_arrays)
+    out_dir = Path(out_dir)
+    if not out_dir.exists():
+        out_dir.mkdir()
+    for idx, arr in out_arrays.items():
+        if np.sum(arr[1:,-1] != "0") / (arr.shape[0] - 1) > 0.8: # do not store if covered more than 80% of the time
+            continue
+        with open(out_dir / f"{idx}.csv", "w") as f:
+            for line in arr:
+                f.write(",".join(map(str, line)) + '\n')
 
 def main():
     parser = argparse.ArgumentParser(prog="parse_coverages",
