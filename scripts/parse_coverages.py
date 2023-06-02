c7c743c7cbab4c7e767a396b3ed300a72bb2ad74
Eran Kohen Behar <erankohen@bwrcrdsl-5.eecs.berkeley.edu>
2023-06-01 18:31:34 -0700

    created combined_scripts_optimized

--- scripts/parse_coverages.py
+++ scripts/parse_coverages.py
@@ -1,9 +1,41 @@
 from pathlib import Path
 import json
 import argparse
-# import pandas as pd
+from pandas import read_csv
 import numpy as np
 from modeling.coverage_parser import parse_line
+
+def parse_and_write_file(config_dir, config_num, seed_num, out_dir):
+    cov_file = Path(config_dir) / f"{config_num}" / f"{seed_num}.cov.dat"
+    conf_file = Path(config_dir) / f"{config_num}" / f"{seed_num}.json"
+    conf_f = open(conf_file, "r")
+    configs = json.load(conf_f)['plusarg_config']
+    config_vals_list = list(configs.values())
+    conf_f.close()
+    f = open(cov_file, "r")
+    lines = f.readlines()[1:]
+    for idx, line in enumerate(lines):
+        count = parse_line(line)[-1]
+        csv_path = out_dir / f"{idx}.csv"
+        if not csv_path.resolve().exists():
+            csv_path.touch()
+            with open(csv_path, "a+") as f:
+                f.write(",".join(list(configs.keys()) + ["count"]) + '\n')
+                f.write(",".join(map(str, config_vals_list + [count])) + '\n')
+        else:
+            with open(csv_path, "a+") as f:
+                f.write(",".join(map(str, config_vals_list + [count])) + '\n')
+    f.close()
+
+def clean_csvs(out_dir, lower_bound, upper_bound):
+    # deletes a csv file if the point was covered in more than upper_bound or less than lower_bound of the tests
+    # lower_ and upper_bound should be [0,1]
+
+    csv_list = list(out_dir.iterdir()) # making a copy to delete csvs while iterating comfortably
+    for csv_dir in csv_list:
+        df = read_csv(csv_dir)
+        if not (lower_bound <= (np.sum(df['count'] > 0) / len(df)) <= upper_bound):
+            csv_dir.unlink()
 
 def parse_file(config_dir, config_num, seed_num, out_arrays):
     cov_file = Path(config_dir) / f"{config_num}" / f"{seed_num}.cov.dat"
